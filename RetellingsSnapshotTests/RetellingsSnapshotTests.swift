41a6398f60b2fbba31026d099b5c9335cbc077e9
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-18 21:52:25 +0300

    Initial project setup. Dependencies, linter, test bundles.

---
+++ RetellingsSnapshotTests/RetellingsSnapshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetellingsSnapshotTests.swift
+//  RetellingsSnapshotTests
+//
+//  Created by VAndrJ on 4/18/25.
+//
+
+import Testing
+
+struct RetellingsSnapshotTests {
+
+    @Test
+    func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
---
+++ RetellingsTests/RetellingsTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetellingsTests.swift
+//  RetellingsTests
+//
+//  Created by VAndrJ on 4/18/25.
+//
+
+import Testing
+
+struct RetellingsTests {
+
+    @Test
+    func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
