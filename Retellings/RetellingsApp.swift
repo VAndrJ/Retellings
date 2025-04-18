1a1bbb7e1d20e4f161cfdaf122269079674cc500
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-18 21:35:44 +0300

    Project creation.

---
+++ Retellings/RetellingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetellingsApp.swift
+//  Retellings
+//
+//  Created by VAndrJ on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RetellingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
