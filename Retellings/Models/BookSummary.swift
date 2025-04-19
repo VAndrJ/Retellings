98882bbd9ea7f32a117e02aca8f221caa69fad73
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-19 13:38:10 +0300

    Models for JSONs.

---
+++ Retellings/Models/BookSummary.swift
@@ -0,0 +1,15 @@
+//
+//  BookSummary.swift
+//  Retellings
+//
+//  Created by VAndrJ on 4/19/25.
+//
+
+import Foundation
+
+struct BookSummary: Decodable {
+    let id: String
+    let title: String
+    let description: String
+    let about: String
+}
---
+++ Retellings/Models/KeyPoint.swift
@@ -0,0 +1,13 @@
+//
+//  KeyPoint.swift
+//  Retellings
+//
+//  Created by VAndrJ on 4/19/25.
+//
+
+import Foundation
+
+struct KeyPoint: Decodable {
+    let title: String
+    let audio: String
+}
