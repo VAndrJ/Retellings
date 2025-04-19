7cdbb2fe414791812657c0ce12052102b02e2757
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-19 13:12:26 +0300

    Reading "view"

--- Retellings/Features/Reading/ReadingView.swift
+++ Retellings/Features/Reading/ReadingView.swift
@@ -13,6 +13,23 @@ struct ReadingView: View {
     @Bindable var store: StoreOf<ReadingReducer>
 
     var body: some View {
-        Text("Hello, world")
+        ScrollView {
+            switch store.state.status {
+            case .idle:
+                LargeProgressView()
+            case let .data(text):
+                Text(text)
+                    .dynamicFont(.inter, size: 17)
+                    .clampingDynamicTypeSize()
+            }
+        }
     }
+}
+
+#Preview {
+    ReadingView(
+        store: Store(initialState: ReadingReducer.State(status: .data("Hello, World!"))) {
+            ReadingReducer()
+        }
+    )
 }
